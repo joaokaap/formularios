92639b6703bfb929b6142fca797d227790ff9bbd
joaokaap <joao.kaap@escola.pr.gov.br>
2026-04-22 20:01:27 +0000

    github

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,110 @@
-import Image from "next/image";
+'use client'
+import React, { useState } from "react";
 
 export default function Home() {
+  // Declarar as variaveis
+  const [nome, setNome] = useState("")
+  const [email, setEmail] = useState("")
+  const [idade, setIdade] = useState("")
+  const [nascimento, setNascimento] = useState("")
+  const [cnh, setCNH] = useState(false)
+  const [altura, setAltura] = useState("")
+  const [cidade, setCidade] = useState("")
+  // Funções
+  function enviarFormulario(e: React.FormEvent) {
+    // React.FormEvent
+    e.preventDefault()
+    console.log(e)
+    alert('Formulario Enviado')
+  }
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+        <form action="" method="" onSubmit={enviarFormulario}>
+          <div>
+            <label htmlFor="nome">Nome</label>
+            <input type="text"
+              placeholder="Digite o seu Nome"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+              name="nome"
+              id="nome"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+          </div>
+          <div>
+            <label htmlFor="E-Mail">Email</label>
+
+            <input type="text"
+              placeholder="Digite o seu E-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              name="email"
+              id="email"
+            />
+          </div>
+          <div>
+            <label htmlFor="Idade">Idade</label>
+
+            <input type="number"
+              placeholder="Digite o sua idade"
+              value={idade}
+              onChange={(e) => setIdade(e.target.value)}
+              name="idade"
+              id="idade"
+            />
+          </div>
+          <div>
+            <label htmlFor="nascimento">Data de Nascimento</label>
+
+            <input type="date"
+              placeholder="Digite o sua data de nascimento"
+              value={nascimento}
+              onChange={(e) => setNascimento(e.target.value)}
+              name="nascimento"
+              id="nascimento"
+            />
+          </div>
+          <div>
+            <label htmlFor="cnh">CNH</label>
+
+            <input type="checkbox"
+              placeholder="Você possui CNH?"
+              checked={cnh}
+              onChange={(e) => setCNH(e.target.checked)}
+              name="cnh"
+              id="cnh"
+            />
+          </div>
+          <div>
+            <label htmlFor="altura">Altura</label>
+
+            <input type="text"
+              placeholder="Digite o sua altura"
+              value={altura}
+              onChange={(e) => setAltura(e.target.value)}
+              name="altura"
+              id="altura"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="cidade">Cidade</label>
+            <select name="cidade" id="cidade" value={cidade}
+              onChange={(e) => setCidade(e.target.value)}>
+              <option value="">Selecione uma Cidade</option>
+              <option value="castro">Castro</option>
+              <option value="tibagi">Tibagi</option>
+              <option value="pirai">Piraí</option>
+
+            </select>
+            <div>
+              <button type="submit">Enviar</button>
+              <button type="reset">Limpar</button>
+            </div>
+          </div>
+
+        </form>
+        <h3>{nome}</h3>
       </main>
     </div>
   );
